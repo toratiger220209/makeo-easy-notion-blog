1124df9ca58c326de60ac22eacc5519249350c9a
toratiger220209 <tora.tiger220209@gmail.com>
2022-12-12 05:22:14 +0900

    Homeページの記述変更

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,22 @@ const RootPage = () => (
     <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
     <div className={styles.container}>
       <div>
-        <h2>Welcome!</h2>
+      {/* <h2>Welcome!</h2>
         <p>Your easy-notion-blog deployed successfully!</p>
         <p>Have fun!</p>
         <p>
           easy-notion-blog powered by{' '}
           <Link href="https://github.com/otoyo/easy-notion-blog">
             otoyo/easy-notion-blog
+          </Link>
+        </p> */}
+      <h2>こんにちは！ こんばんは！</h2>
+        <p>こちらは『まけお』のNotionブログです！</p>
+        <p>Notion好きな非エンジニアが、いろいろやってみるブログです</p>
+        <p>
+          まけおの連絡先はこちら：{' '}
+          <Link href="https://twitter.com/__makeo" target="_blank">
+            Twitterのプロフィール
           </Link>
         </p>
       </div>
